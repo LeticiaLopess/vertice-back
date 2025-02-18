ca9444f847650ccc2d85e755ca5aabaaff8f0451
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2025-02-18 01:39:16 -0300

    cors adjustment

--- config/cors.php
+++ config/cors.php
@@ -6,8 +6,6 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'http://localhost:5176',
-        'http://127.0.0.1:5176'
     ],
     'allowed_headers' => [
         'X-CSRF-TOKEN',
